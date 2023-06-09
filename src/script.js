d62ab672a8e3431436d0851a1732a52be199e8e1
Anchit gupta <79711974+anchitgupta01@users.noreply.github.com>
2023-06-09 22:33:47 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -133,3 +133,36 @@ document
 geocode.getLocation();
 
 
+<<<<<<< HEAD
+=======
+// Your existing weather app code
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Process the news data
+    if (data.articles && Array.isArray(data.articles)) {
+      const articles = data.articles;
+      const newsFeedContainer = document.getElementById('newsFeedContainer');
+
+      // Generate the news feed HTML dynamically
+      let newsHTML = '';
+      articles.forEach(article => {
+        newsHTML += `
+          <div class="news-item">
+            <div class="news-item-title">${article.title}</div>
+            <div class="news-item-description">${article.description}</div>
+          </div>
+        `;
+      });
+
+      // Insert the news feed HTML into the container
+      newsFeedContainer.innerHTML = newsHTML;
+    } else {
+      console.log('No news articles found.');
+    }
+  })
+  .catch(error => {
+    console.log('Error fetching news data:', error);
+  });
+>>>>>>> 2a66ee9 (Update script.js)
